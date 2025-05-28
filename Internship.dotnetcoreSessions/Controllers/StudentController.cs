5d817f4c5f4d0f4419595051adffdfb1712c24df
ashishkhadye33 <ashishkhadye33@gmail.com>
2025-05-28 13:22:02 +0530

    dotnet core session 5

--- Internship.dotnetcoreSessions/Controllers/StudentController.cs
+++ Internship.dotnetcoreSessions/Controllers/StudentController.cs
@@ -34,5 +34,37 @@ namespace Internship.dotnetcoreSessions.Controllers
             return RedirectToAction("Index");
         }
 
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var student = _context.Students.Find(id);
+            return View(student);
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Students student)
+        {
+            _context.Students.Update(student);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var student = _context.Students.Find(id);
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Students student)
+        {
+            _context.Students.Remove(student);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
